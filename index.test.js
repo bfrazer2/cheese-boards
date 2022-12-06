1b5c2b39b8732f9099ea3b7b5638aca0c4423fde
Benjamin Frazer <benjamin.frazer@benjamin.frazer-MacBook-Pro>
2022-12-06 13:54:18 -0500

    boards can have many cheeses and cheeses can have many boards

--- index.test.js
+++ index.test.js
@@ -46,4 +46,28 @@ describe('User, Board & Cheese Associatiosn', () => {
         expect(user1Boards[0].type).toEqual("Soft")
         expect(user1Boards[1].type).toEqual("Hard")
     })
-})
+    
+    test('boards cna have many cheeses & cheeses can have many boards', async () => {
+        await sequelize.sync({force: true})
+        const board1 = await Board.create(seedBoard[0]) //Soft
+        const board2 = await Board.create(seedBoard[2]) //Goat
+        const cheese1 = await Cheese.create(seedCheese[0]) //Hard Goat
+        const cheese2 = await Cheese.create(seedCheese[1]) //Soft Goat
+        const cheese3 = await Cheese.create(seedCheese[2]) //Soft Goat
+
+        await board1.addCheeses([cheese2,cheese3])
+        await board2.addCheeses([cheese1,cheese2,cheese3])
+        await cheese2.addBoards([board1,board2])
+
+        const board1Cheeses = await board1.getCheeses()
+        const cheese2Boards = await cheese2.getBoards()
+
+        expect(board1Cheeses.length).toEqual(2)
+        expect(board1Cheeses[0].title).toEqual("Kesong Puti")
+        expect(board1Cheeses[1].title).toEqual("Anari")
+
+        expect(cheese2Boards.length).toEqual(2)
+        expect(cheese2Boards[0].type).toEqual("Soft")
+        expect(cheese2Boards[1].type).toEqual("Goat")
+    })
+});
