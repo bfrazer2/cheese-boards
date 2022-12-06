ec207cf48aed5ca7dd4385963944e2e7ed834eb5
Benjamin Frazer <benjamin.frazer@benjamin.frazer-MacBook-Pro>
2022-12-06 13:29:37 -0500

    can create board

--- index.test.js
+++ index.test.js
@@ -6,17 +6,21 @@ const {
     seedCheese
   } = require('./seedData');
 
-describe('Restaurant and Menu Models', () => {
+describe('User, Board & Cheese Models', () => {
     beforeAll(async () => {
         await sequelize.sync({ force: true });
     });
 
     test('can create a user', async () => {
-        const user1 = await User.create({
-            name: "Ben",
-            email: "ben@email.com",
-        })
+        const user1 = await User.create(seedUser[0])
         expect(user1.name).toEqual('Ben')
         expect(user1.email).toEqual('ben@email.com')
     });
+
+    test('can create a board', async () => {
+        const board1 = await Board.create(seedBoard[0])
+        expect(board1.type).toEqual("Soft")
+        expect(board1.description).toEqual("Light & Fluffy")
+        expect(board1.rating).toEqual(3)
+    })
 });
