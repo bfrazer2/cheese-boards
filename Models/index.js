ffc885c8a064be372e68e5d1738a78f415a3f41e
Benjamin Frazer <benjamin.frazer@benjamin.frazer-MacBook-Pro>
2022-12-06 13:41:52 -0500

    user can have many boards

--- Models/index.js
+++ Models/index.js
@@ -2,4 +2,10 @@ const {User} = require('./User')
 const {Board} = require('./Board')
 const {Cheese} = require('./Cheese')
 
+Board.belongsTo(User)
+User.hasMany(Board)
+
+Cheese.belongsToMany(Board, {through: "cheese_baord"})
+Board.belongsToMany(Cheese, {through: "cheese_board"})
+
 module.exports = {User,Board,Cheese}
--- index.test.js
+++ index.test.js
@@ -8,7 +8,7 @@ const {
 
 describe('User, Board & Cheese Models', () => {
     beforeAll(async () => {
-        await sequelize.sync({ force: true });
+        await sequelize.sync({ force: true })
     });
 
     test('can create a user', async () => {
@@ -22,11 +22,28 @@ describe('User, Board & Cheese Models', () => {
         expect(board1.type).toEqual("Soft")
         expect(board1.description).toEqual("Light & Fluffy")
         expect(board1.rating).toEqual(3)
-    })
+    });
 
     test('can create a cheese', async () => {
         const cheese1 = await Cheese.create(seedCheese[0])
         expect(cheese1.title).toEqual("Rubing")
         expect(cheese1.description).toEqual("Hard French Goat Cheese")
-    })
+    });
 });
+
+describe('User, Board & Cheese Associatiosn', () => {
+    beforeAll(async () => {
+        await sequelize.sync({force: true})
+    });
+
+    test('user can have many boards', async () => {
+        const user1 = await User.create(seedUser[0])
+        const board1 = await Board.create(seedBoard[0])
+        const board2 = await Board.create(seedBoard[1])
+        await user1.addBoards([board1,board2])
+        const user1Boards = await user1.getBoards()
+        expect(user1Boards.length).toEqual(2)
+        expect(user1Boards[0].type).toEqual("Soft")
+        expect(user1Boards[1].type).toEqual("Hard")
+    })
+})
